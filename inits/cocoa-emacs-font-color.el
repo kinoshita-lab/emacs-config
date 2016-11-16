c69b6ea59016dee422392cd938f434f5b0d48a3c
SAITA Kazuki <saita@kinoshita-lab.org>
2016-11-16 16:01:48 +0900

    tweaking..

--- inits/cocoa-emacs-font-color.el
+++ inits/cocoa-emacs-font-color.el
@@ -7,10 +7,12 @@
                     (tool-bar-mode 0); toolbarいらない
                     (define-key global-map [ns-drag-file] 'ns-find-file)
                     (setq ns-pop-up-frames nil)
-		    ;; font
-					(set-face-attribute 'default nil :family "Rounded M+ 1m" :weight 'light :height 180)
+					;; font
 					 ;; 日本語あああイイイ
-	  (set-fontset-font nil 'japanese-jisx0208
-						(font-spec :family "Rounded M+ 1m light"))
+					(set-fontset-font nil 'japanese-jisx0208
+									  (font-spec :family "Rounded M+ 1m light"))					
+					;(set-face-attribute 'default nil :family "Envy Code B 10pt" :height 180)
+					(set-face-attribute 'default nil :family "Envy Code R" :height 180)
+
                     (setq-default line-spacing .01)
                     ))
--- inits/cocoa-emacs-path.el
+++ inits/cocoa-emacs-path.el
@@ -3,3 +3,4 @@
 (require 'exec-path-from-shell)
 (let ((envs '("PATH" "VIRTUAL_ENV" "GOROOT" "GOPATH")))
   (exec-path-from-shell-copy-envs envs))
+(setq ripgrep-executable "/usr/local/bin/rg")
