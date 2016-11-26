86f8a936a561748ff8165216dfc68be279ae888e
Kaz Saita <saita@kinoshita-lab.org>
2016-11-26 10:07:59 +0900

    tweaking linux

--- inits/70_helm_ivy_etc.el
+++ inits/70_helm_ivy_etc.el
@@ -1,72 +1,72 @@
-; ivy
-(ivy-mode 1)
+;; ; ivy
+;; (ivy-mode 1)
 
-;;; 下記は任意で有効化
-;(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-load-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+;; ;;; 下記は任意で有効化
+;; ;(global-set-key "\C-s" 'swiper)
+;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
+;; (global-set-key (kbd "<f6>") 'ivy-resume)
+;; (global-set-key (kbd "M-x") 'counsel-M-x)
+;; ;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+;; (global-set-key (kbd "<f1> l") 'counsel-load-library)
+;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;; (global-set-key (kbd "C-c g") 'counsel-git)
+;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+;; (global-set-key (kbd "C-c k") 'counsel-ag)
+;; (global-set-key (kbd "C-x l") 'counsel-locate)
+;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;; (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
 
 ; helm
-;; (when (require 'helm-config nil t)
-;;   (require 'helm-descbinds)
+(when (require 'helm-config nil t)
+  (require 'helm-descbinds)
 
-;;   (helm-descbinds-mode)
-;;   (helm-mode 1)
+  (helm-descbinds-mode)
+  (helm-mode 1)
 
-;;   (define-key global-map (kbd "M-x")     'helm-M-x)
-;;   (define-key global-map (kbd "C-x C-f") 'helm-find-files)
-;;   (define-key global-map (kbd "C-x C-r") 'helm-recentf)
-;;   (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
-;;   (define-key global-map (kbd "C-c i")   'helm-imenu)
-;;   (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+  (define-key global-map (kbd "M-x")     'helm-M-x)
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  (define-key global-map (kbd "C-x C-r") 'helm-recentf)
+  (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+  (define-key global-map (kbd "C-c i")   'helm-imenu)
+  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
 
-;;   (define-key helm-map (kbd "C-h") 'delete-backward-char)
-;;   (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-;;   (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-;;   (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
-;;   (global-set-key (kbd "C-;") 'helm-mini)
-;;   (global-set-key (kbd "C-c h") 'helm-mini)
-;;   (global-set-key (kbd "C-c b") 'helm-descbinds)
-;;   (global-set-key (kbd "C-c o") 'helm-occur)
-;;   (global-set-key (kbd "C-c y") 'helm-show-kill-ring)
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+  (global-set-key (kbd "C-;") 'helm-mini)
+  (global-set-key (kbd "C-c h") 'helm-mini)
+  (global-set-key (kbd "C-c b") 'helm-descbinds)
+  (global-set-key (kbd "C-c o") 'helm-occur)
+  (global-set-key (kbd "C-c y") 'helm-show-kill-ring)
 
-;;   ;; Disable helm in some functions
-;;   (add-to-list 'helm-completing-read-handlers-alist '(find-alternate-file . nil))
+  ;; Disable helm in some functions
+  (add-to-list 'helm-completing-read-handlers-alist '(find-alternate-file . nil))
 
-;;   ;; Emulate `kill-line' in helm minibuffer
-;;   (setq helm-delete-minibuffer-contents-from-point t)
-;;   (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
-;;     "Emulate `kill-line' in helm minibuffer"
-;;     (kill-new (buffer-substring (point) (field-end))))
+  ;; Emulate `kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
 
-;;   (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
-;;     "Execute command only if CANDIDATE exists"
-;;     (when (file-exists-p candidate)
-;;       ad-do-it))
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it))
 
-;;   (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
-;;     "Transform the pattern to reflect my intention"
-;;     (let* ((pattern (ad-get-arg 0))
-;;            (input-pattern (file-name-nondirectory pattern))
-;;            (dirname (file-name-directory pattern)))
-;;       (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
-;;       (setq ad-return-value
-;;             (concat dirname
-;;                     (if (string-match "^\\^" input-pattern)
-;;                         ;; '^' is a pattern for basename
-;;                         ;; and not required because the directory name is prepended
-;;                         (substring input-pattern 1)
-;;                       (concat ".*" input-pattern)))))))
+  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
+    "Transform the pattern to reflect my intention"
+    (let* ((pattern (ad-get-arg 0))
+           (input-pattern (file-name-nondirectory pattern))
+           (dirname (file-name-directory pattern)))
+      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
+      (setq ad-return-value
+            (concat dirname
+                    (if (string-match "^\\^" input-pattern)
+                        ;; '^' is a pattern for basename
+                        ;; and not required because the directory name is prepended
+                        (substring input-pattern 1)
+                      (concat ".*" input-pattern)))))))
