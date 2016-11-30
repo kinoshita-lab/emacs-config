769628bddce77a986f4129339b016804a1a8262f
SAITA Kazuki <saita@kinoshita-lab.org>
2016-11-30 23:19:50 +0900

    merge

--- inits/70_helm_ivy_etc.el
+++ inits/70_helm_ivy_etc.el
@@ -1,32 +1,3 @@
-; ivy
-;;(ivy-mode 1)
-
-;;; 下記は任意で有効化
-<<<<<<< HEAD
-;; ;(global-set-key "\C-s" 'swiper)
-=======
-;(global-set-key "\C-s" 'swiper)
->>>>>>> 5d3769f3d951b40e61eed52757983d6a6fffd273
-;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
-;; (global-set-key (kbd "<f6>") 'ivy-resume)
-;; (global-set-key (kbd "M-x") 'counsel-M-x)
-;; ;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-;; (global-set-key (kbd "<f1> l") 'counsel-load-library)
-;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-;; (global-set-key (kbd "C-c g") 'counsel-git)
-;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
-;; (global-set-key (kbd "C-c k") 'counsel-ag)
-;; (global-set-key (kbd "C-x l") 'counsel-locate)
-;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-<<<<<<< HEAD
-;; (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-=======
-;; (define-key read-expression-map (kbd "C-r") 'counsel-expression-history) 
->>>>>>> 5d3769f3d951b40e61eed52757983d6a6fffd273
-
 ; helm
 (when (require 'helm-config nil t)
   (require 'helm-descbinds)
