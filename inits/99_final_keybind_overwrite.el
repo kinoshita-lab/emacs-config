89748908ab2bb4cbffdf38d9f6ffdd94c3f7c5a5
kazbo <saita@kinoshita-lab.org>
2016-12-01 11:09:43 +0900

    modify tabber key binds

--- inits/99_final_keybind_overwrite.el
+++ inits/99_final_keybind_overwrite.el
@@ -1,8 +1,11 @@
 ;; overwrite DWIM key bindings!!!
 (require 'bind-key)
 ;; tabber
-(bind-key* (kbd "M-<right>") 'tabbar-forward-tab)
-(bind-key* (kbd "M-<left>") 'tabbar-backward-tab)
+(bind-key* (kbd "C-M-<right>") 'tabbar-forward-tab)
+(bind-key* (kbd "C-M-<left>") 'tabbar-backward-tab)
+
 
 ;; eclipse-like header/impl switcher
 (bind-key* (kbd "<C-tab>") 'ff-find-other-file)
+
+
